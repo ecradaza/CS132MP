b71ad3ac3271f52192f49331c3d4d453e4bc3756
alliahc <71240608+alliahc@users.noreply.github.com>
2022-05-25 08:44:51 +0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,10 @@
+import tweepy
+import logging
+import json
+from datetime import datetime
+import random
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler("9BIQxQx1gg3DLVlU3bHBeHRjC", "ZIxQ029NBCkcGwbhmgPwKLC11psfNfuSALeBSvvQTlkgcyCtt4")
+auth.set_access_token("1417308731725942791-jf347s0CqwVzVk1Ri85lsbazkdf1bx", "bHQ5n6GxPYq8LHB9368k0kxPYPLeIySrwMt76X5fFmrVg")
+
+#bot main
