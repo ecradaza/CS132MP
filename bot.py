236560d9d39796dbe97f6a1a96d9da3183d63991
alliahc <71240608+alliahc@users.noreply.github.com>
2022-05-25 08:50:34 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,5 +6,5 @@ import random
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler("9BIQxQx1gg3DLVlU3bHBeHRjC", "ZIxQ029NBCkcGwbhmgPwKLC11psfNfuSALeBSvvQTlkgcyCtt4")
 auth.set_access_token("1417308731725942791-jf347s0CqwVzVk1Ri85lsbazkdf1bx", "bHQ5n6GxPYq8LHB9368k0kxPYPLeIySrwMt76X5fFmrVg")
-
+api = tweepy.API(auth)
 #bot main
